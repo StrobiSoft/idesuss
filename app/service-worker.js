21a8ccfa959edd344e9876c9f64a69713736bbb5
StrobiSoft <strobelzoltan@gmail.com>
2026-04-19 10:35:53 +0200

    Update service-worker.js

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,1 +1,22 @@
-.
+const CACHE_NAME = "idesuss-v1";
+
+const urlsToCache = [
+  "/app/",
+  "/app/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
