3e4928aa34397be5f3bc47ed2e69ab2aae9f9542
StZoo <strobelzoltan@gmail.com>
2026-09-23 09:45:43 +0200

    chore: bump avatar moderation module versions

--- js/menu/menu-core.js
+++ js/menu/menu-core.js
@@ -1,5 +1,5 @@
-import { initRootAuthController } from "./auth-controller.js?v=20260923-passwordsec1";
-import { openProfilePanel } from "./profile.js?v=20260923-staff-avatar1";
+import { initRootAuthController } from "./auth-controller.js?v=20260923-avatar-review1";
+import { openProfilePanel } from "./profile.js?v=20260923-avatar-review1";
 import { initSettingsPreferences, openSettingsPanel } from "./settings.js?v=20260923-finalweb1";
 
 function initFloatingMenu() {
