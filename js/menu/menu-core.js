dc8676c17ce7ae2393bf077bdc219cdb426c3f5c
StZoo <strobelzoltan@gmail.com>
2026-09-24 14:29:21 +0200

    chore: refresh auth controller bundle version

--- js/menu/menu-core.js
+++ js/menu/menu-core.js
@@ -1,4 +1,4 @@
-import { initRootAuthController } from "./auth-controller.js?v=20260923-avatar-review1";
+import { initRootAuthController } from "./auth-controller.js?v=20260924-vip-presence1";
 import { openProfilePanel } from "./profile.js?v=20260923-eula1";
 import { initSettingsPreferences, openSettingsPanel } from "./settings.js?v=20260923-finalweb1";
 
